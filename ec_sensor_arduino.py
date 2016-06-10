4fff9f4c384530860e93b378e4427500a4adc54e
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-10 13:02:29 -0400

    Updated with a working model; still needs refactor

--- ec_sensor_arduino.py
+++ ec_sensor_arduino.py
@@ -1,72 +1,73 @@
-# from threading import Thread
-# import serial
+from threading import Thread
+import serial
 import requests
 import time
 
-# last_received = ''
-#
-#
-# def receiving(ser):
-#     global last_received
-#     buffer = ''
-#     while ser.isOpen():
-#         buffer = buffer + ser.read(ser.inWaiting()).decode('utf-8')
-#         if '\n' in buffer:
-#             lines = buffer.split('\n')  # Guaranteed to have at least 2 entries
-#             last_received = lines[-2]
-#             # If the Arduino sends lots of empty lines, you'll lose the
-#             # last filled line, so you could make above statement conditional
-#             # like so: if lines[-2]: last_received = lines[-2]
-#             buffer = lines[-1]
-#
-#
-# class SerialData(object):
-#     def __init__(self, init=50):
-#         try:
-#             self.ser = serial.Serial(
-#                 port='/dev/cu.usbmodem1421',
-#                 baudrate=9600,
-#                 bytesize=serial.EIGHTBITS,
-#                 parity=serial.PARITY_NONE,
-#                 stopbits=serial.STOPBITS_ONE,
-#                 timeout=0.1,
-#                 xonxoff=0,
-#                 rtscts=0,
-#                 interCharTimeout=None
-#             )
-#         except serial.serialutil.SerialException:
-#             # no serial connection
-#             self.ser = None
-#         else:
-#             thread = Thread(target=receiving, args=(self.ser,))
-#             thread.daemon = True
-#             thread.start()
-#
-#     def next(self):
-#         if not self.ser:
-#             return -1000  # return so we can test when Arduino isn't connected
-#         # return a float value or try a few times until we get one
-#         for i in range(40):
-#             raw_line = last_received
-#             try:
-#                 return float(raw_line.strip())
-#             except ValueError:
-#                 # print('bogus data ', raw_line)
-#                 time.sleep(.005)
-#         return 0
-#
-#     def __del__(self):
-#         if self.ser:
-#             self.ser.close()
+last_received = ''
+
+
+def receiving(ser):
+    global last_received
+    buffer = ''
+    while ser.isOpen():
+        buffer = buffer + ser.read(ser.inWaiting()).decode('utf-8')
+        if '\n' in buffer:
+            lines = buffer.split('\n')  # Guaranteed to have at least 2 entries
+            last_received = lines[-2]
+            # If the Arduino sends lots of empty lines, you'll lose the
+            # last filled line, so you could make above statement conditional
+            # like so: if lines[-2]: last_received = lines[-2]
+            buffer = lines[-1]
+
+
+class SerialData(object):
+    def __init__(self, init=50):
+        try:
+            self.ser = serial.Serial(
+                port='/dev/cu.usbmodem1421',
+                baudrate=9600,
+                bytesize=serial.EIGHTBITS,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                timeout=0.1,
+                xonxoff=0,
+                rtscts=0,
+                interCharTimeout=None
+            )
+        except serial.serialutil.SerialException:
+            # no serial connection
+            self.ser = None
+        else:
+            thread = Thread(target=receiving, args=(self.ser,))
+            thread.daemon = True
+            thread.start()
+
+    def next(self):
+        if not self.ser:
+            return -1000  # return so we can test when Arduino isn't connected
+        # return a float value or try a few times until we get one
+        for i in range(40):
+            raw_line = last_received
+            try:
+                float(raw_line[1:].strip())
+                return raw_line
+            except ValueError:
+                # print('bogus data ', raw_line)
+                time.sleep(.005)
+        return 0
+
+    def __del__(self):
+        if self.ser:
+            self.ser.close()
 
 
 def main():
-    # sleep_time = 1
-    # # serial_data = SerialData()
-    # time.sleep(3)
+    sleep_time = 1
+    serial_data = SerialData()
+    time.sleep(3)
     base_url = 'http://bonapetite.herokuapp.com/'
     with requests.Session() as s:
-        url = 'http://localhost:5000/login/'
+        url = base_url + 'login/'
         s.get(url)
         csrftoken = s.cookies['csrftoken']
 
@@ -77,20 +78,31 @@ def main():
 
         s.post(url, data=login_payload, headers=headers)
 
-        for i in range(1):
-            time.sleep(1)
-            # while True:
-            # time.sleep(sleep_time)
-            # pH_level = serial_data.next()
-            url = 'http://localhost:5000/api/mister'
+        for i in range(2):
+            ec_level, temperature = '', ''
+            time.sleep(sleep_time)
+            raw_value = serial_data.next()
+            if 'E' in raw_value:
+                ec_level = float(raw_value[1:])
+            if 'T' in raw_value:
+                temperature = float(raw_value[1:])
+            while not ec_level:
+                raw_value = serial_data.next()
+                if 'E' in raw_value:
+                    ec_level = float(raw_value[1:])
+            while not temperature:
+                raw_value = serial_data.next()
+                if 'T' in raw_value:
+                    temperature = float(raw_value[1:])
+            url = base_url + 'api/mister'
             s.get(url)
             print('csrf: {}'.format(s.cookies['csrftoken']))
             csrftoken = s.cookies['csrftoken']
 
-            sensor_payload = {'pH_level': 1111.0,
-                              'temperature': 1234.0,
+            sensor_payload = {'pH_level': ec_level,
+                              'temperature': temperature,
                               'csrfmiddlewaretoken': csrftoken}
-            r = s.post('http://localhost:5000/api/mister/',
+            r = s.post(base_url + 'api/mister/',
                        data=sensor_payload)
             print(r.status_code)
 
